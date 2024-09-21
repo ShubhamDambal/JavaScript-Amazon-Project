f859d76c5a02a3b3aa92b2cb998ef6ebef939f99
ShubhamDambal <shubhamdambal@gmail.com>
2024-09-21 19:36:01 +0530

    Move addToCart() to cart.js file

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,30 @@
 export const cart = [];
+
+export function addToCart(productId){
+  let matchingItem;  //To keep track of same products by different brands
+
+    //traverse cart to check item already preset or not
+    cart.forEach((cartItem) => {
+      if(productId === cartItem.productId){
+        matchingItem = cartItem;
+      }
+    });
+
+    const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+
+    //converting into Number() bcz DOM gives string by default
+    const quantity = Number(quantitySelector.value);
+
+    //Adding product to the cart
+    if(matchingItem){
+      matchingItem.quantity += quantity;
+    }
+    else{
+      cart.push({
+        // productId: productId,
+        // quantity: quantity
+        productId,
+        quantity
+      });
+    }
+}
