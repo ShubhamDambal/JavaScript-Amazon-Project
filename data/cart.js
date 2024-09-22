d55adc722cf94540f5e31f0512ff14f5b6aa6180
ShubhamDambal <shubhamdambal@gmail.com>
2024-09-22 18:15:36 +0530

    Deletes product from cart when clicking delete.

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [{
+export let cart = [{
   productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
   quantity: 2
 },
@@ -34,4 +34,20 @@ export function addToCart(productId){
         quantity
       });
     }
+}
+
+export function removeFromCart(productId){
+  //steps: 1)Create new arr
+         //2)Traverse the cart(products which are added to cart)
+         //3)Add prodt to new arr iff product id not mathces
+
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if(cartItem.productId !== productId){
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart; //put newCart webpage
 }
